270ac3f527b319834ae8b220743f91e3a16ecaac
New Developer <slenka528@gmail.com>
2023-01-22 23:24:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let textEl=document.getElementById("text-el");
+let btnEl=document.firstElementChild("save-btn");
+
+btnEl.addEventListener("click",function(){
+    console.log("button is clicked")
+})
+
